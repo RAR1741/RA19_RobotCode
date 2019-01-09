478feb103e0e4809a8fb533c41e4d8e9b894eb8e
iCodeCoolStuff <rodriguezisaac799@gmail.com>
2019-01-08 20:33:54 -0500

    make the compressor not static anymore

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -24,7 +24,7 @@ public class Robot extends TimedRobot {
   private static final String kCustomAuto = "My Auto";
   private String m_autoSelected;
   private final SendableChooser<String> m_chooser = new SendableChooser<>();
-  private static Compressor compressor;
+  private Compressor compressor;
 
   /**
    * This function is run when the robot is first started up and should be
