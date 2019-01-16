a6a7ab87f9cddab3e662b69497ab8c0472ce3536
Patrick Frampton <pframpton@salesforce.com>
2019-01-16 14:32:36 -0500

    Add new halfway test

--- src/test/java/frc/robot/PressureSensorTest.java
+++ src/test/java/frc/robot/PressureSensorTest.java
@@ -9,23 +9,34 @@ import org.junit.Test;
 import edu.wpi.first.wpilibj.AnalogInput;
 
 public class PressureSensorTest {
+  // Floating point math is inexeact, agreeing to 1x10^-3 should be
+  // good enough.
   private static final double TOLERANCE = 1e-3;
+
+  private AnalogInput getFakeInput(double voltageToReturn) {
+    AnalogInput fakeInput = mock(AnalogInput.class);
+    when(fakeInput.getVoltage()).thenReturn(voltageToReturn);
+    return fakeInput;
+  }
 
   @Test
   public void testGetPressureEmpty() {
-    AnalogInput fakeInput = mock(AnalogInput.class);
-    when(fakeInput.getVoltage()).thenReturn(0.5);
-
+    AnalogInput fakeInput = getFakeInput(0.5);
     PressureSensor sensor = new PressureSensor(fakeInput);
     assertEquals(0.0, sensor.getPressure(), TOLERANCE);
   }
 
   @Test
   public void testGetPressureFull() {
-    AnalogInput fakeInput = mock(AnalogInput.class);
-    when (fakeInput.getVoltage()).thenReturn(2.70);
-
+    AnalogInput fakeInput = getFakeInput(2.70);
     PressureSensor sensor = new PressureSensor(fakeInput);
     assertEquals(120.0, sensor.getPressure(), TOLERANCE);
+  }
+
+  @Test
+  public void testGetPressureHalf() {
+    AnalogInput fakeInput = getFakeInput(1.6);
+    PressureSensor sensor = new PressureSensor(fakeInput);
+    assertEquals(60.0, sensor.getPressure(), TOLERANCE);
   }
 }
