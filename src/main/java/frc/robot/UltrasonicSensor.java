efdd6d7c8c343f1825c3d45adb6ef3ac4f09f2b8
ultragamer1010 <31806444+ultragamer1010@users.noreply.github.com>
2019-01-26 14:19:23 -0500

    Appease Code Climate

--- src/main/java/frc/robot/UltrasonicSensor.java
+++ src/main/java/frc/robot/UltrasonicSensor.java
@@ -9,10 +9,6 @@ public class UltrasonicSensor {
     this.input = input;
   }
 
-  // private static final double VOLTAGE_AT_ZERO = 0.0;
-  // private static final double VOLTAGE_AT_MAX = 5.0;
-  // private static final double MAX_DISTANCE = 450.0;
-
   /**
    * Get current pressure in PSI.
    * @return double pressure in PSI.
@@ -21,8 +17,6 @@ public class UltrasonicSensor {
     double voltage = input.getVoltage();
 
     double distance = voltage/2;
-    // double range = VOLTAGE_AT_MAX - VOLTAGE_AT_ZERO;
-    // double distance = (voltage - VOLTAGE_AT_ZERO) * MAX_DISTANCE / range;
 
     return distance;
   }
