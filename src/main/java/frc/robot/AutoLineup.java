5920a04e3afa9d01623430774a9c49ab1db3b547
ultragamer1010 <31806444+ultragamer1010@users.noreply.github.com>
2019-02-04 19:34:51 -0500

    add auto state machine implementation

--- src/main/java/frc/robot/AutoLineup.java
+++ src/main/java/frc/robot/AutoLineup.java
@@ -1,17 +1,82 @@
 package frc.robot;
 
+import frc.robot.Drivetrain;
+
 public class AutoLineup {
 
   private final int IMG_WIDTH = 640;
   private final int CAM_FOV = 60;
   private final double TARGET_DISTANCE = 200;
+  private final double MAX_MOTOR_PERCENT = 0.8;
 
-  public enum AutoLineupState{
+  private double P;
+  private double motorPower;
+  private double error;
+
+  public enum AutoLineupState {
     LINING_UP,
+    LINING_UP_2,
     DRIVING,
-    TURNING
+    TURNING,
+    IDLE; 
+  }
+  private AutoLineupState state;
+  private Drivetrain drive;
+
+  public AutoLineup(Drivetrain drive){
+    state = AutoLineupState.LINING_UP;
+    this.drive = drive;
   }
 
+  public void run() {
+    switch(state) {
+      case LINING_UP:
+        error = getCameraDegree(getTurn(centerX))/2*P;
+        motorPower = MAX_MOTOR_PERCENT * P;
+        drive.driveLeft(motorPower);
+        drive.driveRight(-motorPower);
+        if (getCameraDegree(getTurn(centerX)) >= -1 || getCameraDegree(getTurn(centerX)) <= 1){
+          double directDistance = getDistance();
+          state = AutoLineupState.LINING_UP_2;
+        }
+        break;
+
+
+      case LINING_UP_2:
+        error = (getTurnDegree() - getRoll()) * P;
+        motorPower = MAX_MOTOR_PERCENT * P;
+        drive.driveLeft(motorPower);
+        drive.driveRight(-motorPower);
+        if (error >= -1 || error  <= 1){
+          state = AutoLineupState.DRIVING;
+        }
+        break;
+
+      case DRIVING:
+        error = (getPathDistance() - getDistance()) * P;
+        motorPower = MAX_MOTOR_PERCENT * P;
+        drive.driveLeft(motorPower);
+        drive.driveRight(motorPower);
+        if (error >= -1 || error <= 1){
+          state = AutoLineupState.TURNING;
+        }
+        break;
+
+      case TURNING:
+        error = (90 - getRoll()) * P;
+        motorPower = MAX_MOTOR_PERCENT * P;
+        drive.driveLeft(motorPower);
+        drive.driveRight(-motorPower);
+        if (error >= -1 || error <= 1){
+          state = AutoLineupState.IDLE;
+        }
+        break;
+
+      case IDLE:
+        break;
+    }
+  }
+  
   /*
     double turn = centerX - (IMG_WIDTH/2);
     double cameraDegree = turn/(IMG_WIDTH/CAM_FOV);
@@ -29,7 +94,7 @@ public class AutoLineup {
    * @return the number of pixels from the center of the camera
    */
   public double getTurn(double centerX) {
-    return centerX - (IMG_WIDTH/2);    
+    return centerX - (IMG_WIDTH/2);
   }
 
   /**
@@ -43,25 +108,14 @@ public class AutoLineup {
   }
 
   /**
-   * Gets the degrees of the robot to the target compared to the field.
-   * 
-   * @param cameraDegree the number of degrees from the center of the camera's vision
-   * @param gyroDegree the number of degrees from the gyro sensor
-   * @return the number of degrees of the robot to the target compared to the field
-   */
-  public double getGlobalDegree(double cameraDegree, double gyroDegree){
-    return gyroDegree + cameraDegree;
-  }
-
-  /**
    * Gets the distance in centimeters from the robot to a point.
    * 
    * @param globalDegree the number of degrees of the robot to the target compared to the field
    * @param directDistance the number of centimeters from the robot to the target
    * @return the distance in centimeters from the robot to a point
    */
-  public double getA(double globalDegree, double directDistance){
-    return Math.cos(globalDegree)*directDistance;
+  public double getA(double cameraDegree, double directDistance){
+    return Math.cos(cameraDegree)*directDistance;
   }
 
   /**
