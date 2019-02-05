e97ddebe6baca6644f53a623a260d76124cc0ed3
ultragamer1010 <31806444+ultragamer1010@users.noreply.github.com>
2019-02-04 20:16:33 -0500

    Updated state machine and methods

--- src/main/java/frc/robot/AutoLineup.java
+++ src/main/java/frc/robot/AutoLineup.java
@@ -31,11 +31,11 @@ public class AutoLineup {
   public void run() {
     switch(state) {
       case LINING_UP:
-        error = getCameraDegree(getTurn(centerX))/2*P;
+        error = getCameraDegree(centerX)/2*P;
         motorPower = MAX_MOTOR_PERCENT * P;
         drive.driveLeft(motorPower);
         drive.driveRight(-motorPower);
-        if (getCameraDegree(getTurn(centerX)) >= -1 || getCameraDegree(getTurn(centerX)) <= 1){
+        if (getCameraDegree(centerX) >= -1 || getCameraDegree(centerX) <= 1){
           double directDistance = getDistance();
           state = AutoLineupState.LINING_UP_2;
         }
@@ -43,7 +43,7 @@ public class AutoLineup {
 
 
       case LINING_UP_2:
-        error = (getTurnDegree() - getRoll()) * P;
+        error = (getTurnDegree(centerX, getDistance()) - getRoll()) * P;
         motorPower = MAX_MOTOR_PERCENT * P;
         drive.driveLeft(motorPower);
         drive.driveRight(-motorPower);
@@ -53,7 +53,7 @@ public class AutoLineup {
         break;
 
       case DRIVING:
-        error = (getPathDistance() - getDistance()) * P;
+        error = (getPathDistance(centerX, getDistance()) - getDistance()) * P;
         motorPower = MAX_MOTOR_PERCENT * P;
         drive.driveLeft(motorPower);
         drive.driveRight(motorPower);
@@ -88,66 +88,44 @@ public class AutoLineup {
   */
 
   /**
-   * Gets the number of pixels from the center of the camera.
-   * 
-   * @param centerX The center of the vision target
-   * @return the number of pixels from the center of the camera
-   */
-  public double getTurn(double centerX) {
-    return centerX - (IMG_WIDTH/2);
-  }
-
-  /**
    * Gets the degrees from the center of the camera's vision.
    * 
    * @param turn the number of pixels from the center of the camera
+   * @param centerX The center of the vision target
    * @return the number of degrees from the center of the camera's vision
    */
-  public double getCameraDegree(double turn) {
+  public double getCameraDegree(double centerX) {
+    double turn = centerX - (IMG_WIDTH/2);
     return turn/(IMG_WIDTH/CAM_FOV);
   }
 
   /**
-   * Gets the distance in centimeters from the robot to a point.
+   * Gets the length in centimeters of the path to a point TARGET_DISTANCE away from the target.\
    * 
-   * @param globalDegree the number of degrees of the robot to the target compared to the field
+   * @param centerX The center of the vision target
    * @param directDistance the number of centimeters from the robot to the target
-   * @return the distance in centimeters from the robot to a point
    */
-  public double getA(double cameraDegree, double directDistance){
-    return Math.cos(cameraDegree)*directDistance;
-  }
-
-  /**
-   * Gets the distance in centimeters from a point to the target.
-   * 
-   * @param directDistance the number of centimeters from the robot to the target
-   * @param a the distance in centimeters from the robot to a point
-   * @return the distance in centimeters from a point to the target
-   */
-  public double getB(double directDistance, double a){
-    return Math.sqrt(Math.pow(directDistance,2) - Math.pow(a,2));
-  }
-
-  /**
-   * Gets the length in centimeters of the path to a point TARGET_DISTANCE away from the target.
-   * 
-   * @param a the distance in centimeters from the robot to a point
-   * @param b the distance in centimeters from a point to the target
-   * @return the length in centimeters of the path to a point TARGET_DISTANCE away from the target
-   */
-  public double getPathDistance(double a, double b) {
+  public double getPathDistance(double centerX, double directDistance) {
+    double turn = centerX - (IMG_WIDTH/2);
+    double cameraDegree = turn/(IMG_WIDTH/CAM_FOV);
+    double a = Math.cos(cameraDegree)*directDistance;
+    double b = Math.sqrt(Math.pow(directDistance,2) - Math.pow(a,2));
     return Math.sqrt(Math.pow(a,2) + Math.pow((b - TARGET_DISTANCE),2));
   }
 
   /**
    * Gets the global degrees the robot must turn to in order to line up with the path.
    * 
-   * @param a the distance in centimeters from the robot to a point
-   * @param pathDistance the length in centimeters of the path to a point TARGET_DISTANCE away from the target
+   * @param centerX The center of the vision target
+   * @param directDistance the number of centimeters from the robot to the target
    * @return the global degrees the robot must turn to in order to line up with the path
    */
-  public double getTurnDegree(double a, double pathDistance){
+  public double getTurnDegree(double centerX, double directDistance){
+    double turn = centerX - (IMG_WIDTH/2);
+    double cameraDegree = turn/(IMG_WIDTH/CAM_FOV);
+    double a = Math.cos(cameraDegree)*directDistance;
+    double b = Math.sqrt(Math.pow(directDistance,2) - Math.pow(a,2));
+    double pathDistance = Math.sqrt(Math.pow(a,2) + Math.pow((b - TARGET_DISTANCE),2));
     return Math.acos(a/pathDistance);
   }
 }
