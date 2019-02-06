2681eda5e7304dd4eb7f75cd8c4d475e9e7bf943
Jordan Miller <jmiller@springbuk.com>
2019-02-05 20:55:03 -0500

    GAH so away pressure sensor

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -192,7 +192,7 @@ public class Robot extends TimedRobot {
   @Override
   public void teleopPeriodic() {
     // Run teleop code (interpreting input, etc.)
-    System.out.println(String.format("Pressure: %2.2f", pressureSensor.getPressure()));
+    // System.out.println(String.format("Pressure: %2.2f", pressureSensor.getPressure()));
     drive.arcadeDrive(xbc.getX(GenericHID.Hand.kLeft),
                       xbc.getY(GenericHID.Hand.kLeft));
     dataLogger.log("timer", timer.get());
