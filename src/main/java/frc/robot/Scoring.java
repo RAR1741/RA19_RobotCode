96845ff23eec762672fdec6b46bd1f8f02cd8c1e
iCodeCoolStuff <rodriguezisaac799@gmail.com>
2019-02-06 20:07:09 -0500

    add some scoring changes

--- src/main/java/frc/robot/Scoring.java
+++ src/main/java/frc/robot/Scoring.java
@@ -17,40 +17,55 @@ public class Scoring {
      * 
      * @param rollerCanId     The CAN id of the talon for the roller motor
      * @param rotateCanId     The CAN id of the talon for the rotater motor
+     * @param tiltCanId       The CAN id of the talon for the tilt motor
+     *
+     * #TODO find some better name for "tiltMotor" cuz that's not what it does.
+     *
      * @param pushersCanId    The CAN id of the pcm for the pushers
      * @param pushersChannel1 The channel number of the forward channel for the pushers
      * @param pushersChannel2 The channel number of the reverse channel for the pushers
      */
-    Scoring(int rollerCanId, int rotaterCanId, int pushersCanId, int pushersChannel1, int pushersChannel2) {
+    Scoring(int rollerCanId, int rotaterCanId, int tiltCanId, int pushersCanId, int pushersChannel1, int pushersChannel2) {
         rollerTalon    = new WPI_TalonSRX(rollerCanId);
         rotaterTalon   = new WPI_TalonSRX(rotaterCanId);
+        tiltTalon      = new WPI_TalonSRX(tiltCanId);
         pushers        = new DoubleSolenoid(pushersCanId, pushersChannel1, pushersChannel2);
     }
 
     /**
      * Rotates the roller either forwards or backwards.
-     * @param speed the speed to rotate the motor (ranges from -1.0 to 1.0)
+     * @param speed the speed to rotate the roller motor (ranges from -1.0 to 1.0)
      */
     public void roll(double speed) {
         rollerTalon.set(ControlMode.PercentOutput, speed);
     }
 
     /**
-     * Pushes the pushers forwards.
+     * Pushes the pneumatic actuators forwards.
      */
     public void push() {
         pushers.set(DoubleSolenoid.Value.kForward);
     }
 
     /**
-     * Retracts the pushers.
+     * Pushes the pneumatic actuators back in
      */
-    public void retract() {
-        pushers.set(DoubleSolenoid.Value.kReverse);
+    public void insertNameHerePls() {
+        pushers.set(DoubleSolenoid.Value.kForward);
     }
 
     /**
-     * Rotates the rotater
+     * Extends the scoring subsystem
+     */
+    public void extend() {}
+
+    /**
+     * Retracts the scoring subsystem
+     */
+    public void retract() {}
+
+    /**
+     * Rotates the intake motor
      * @param speed the speed to rotate the rotater (ranges from -1.0 to 1.0)
      */
     public void rotate(double speed) {
