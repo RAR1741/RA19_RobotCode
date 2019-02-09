1121ea1d6d9038d362bf976b43d0efc258871d87
ultragamer1010 <31806444+ultragamer1010@users.noreply.github.com>
2019-02-09 17:43:37 -0500

    Added second back double solenoid

--- src/main/java/frc/robot/DrivetrainLift.java
+++ src/main/java/frc/robot/DrivetrainLift.java
@@ -1,16 +1,16 @@
 package frc.robot;
 
+import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 
 import edu.wpi.first.wpilibj.DoubleSolenoid;
-
-import com.ctre.phoenix.motorcontrol.ControlMode;
 
 public class DrivetrainLift {
 
   private DoubleSolenoid frontLiftL;
   private DoubleSolenoid frontLiftR;
-  private DoubleSolenoid backLift;
+  private DoubleSolenoid backLiftL;
+  private DoubleSolenoid backLiftR;
 
   private WPI_TalonSRX rollerTalon;
 
@@ -27,15 +27,20 @@ public class DrivetrainLift {
    * @param frontLiftRChannel1 The channel number of the forward channel for the front right lift
    * @param frontLiftRChannel2 The channel number of the reverse channel for the front right lift
    *
-   * @param backLiftCanId The CAN id of the pcm for the back lift
-   * @param backLiftChannel1 The channel number of the forward channel for the back lift
-   * @param backLiftChannel2 The channel number of the reverse channel for the back lift
+   * @param backLiftLCanId The CAN id of the pcm for the back left lift
+   * @param backLiftLChannel1 The channel number of the forward channel for the back left lift
+   * @param backLiftLChannel2 The channel number of the reverse channel for the back left lift
+   *
+   * @param backLiftRCanId The CAN id of the pcm for the back right lift
+   * @param backLiftRChannel1 The channel number of the forward channel for the back right lift
+   * @param backLiftRChannel2 The channel number of the reverse channel for the back right lift
    */
-  DrivetrainLift(int rollerCanId, int frontLiftLCanId, int frontLiftLChannel1, int frontLiftLChannel2, int frontLiftRCanId, int frontLiftRChannel1, int frontLiftRChannel2, int backLiftCanId, int backLiftChannel1, int backLiftChannel2) {
+  DrivetrainLift(int rollerCanId, int frontLiftLCanId, int frontLiftLChannel1, int frontLiftLChannel2, int frontLiftRCanId, int frontLiftRChannel1, int frontLiftRChannel2, int backLiftLCanId, int backLiftLChannel1, int backLiftLChannel2, int backLiftRCanId, int backLiftRChannel1, int backLiftRChannel2) {
     rollerTalon = new WPI_TalonSRX(rollerCanId);
     frontLiftL = new DoubleSolenoid(frontLiftLCanId, frontLiftLChannel1, frontLiftLChannel2);
     frontLiftR = new DoubleSolenoid(frontLiftRCanId, frontLiftRChannel1, frontLiftRChannel2);
-    backLift = new DoubleSolenoid(backLiftCanId, backLiftChannel1, backLiftChannel2);
+    backLiftL = new DoubleSolenoid(backLiftLCanId, backLiftLChannel1, backLiftLChannel2);
+    backLiftR = new DoubleSolenoid(backLiftRCanId, backLiftRChannel1, backLiftRChannel2);
   }
 
   /**
@@ -47,7 +52,7 @@ public class DrivetrainLift {
   }
 
   /**
-   * Pulles front lift in
+   * Pulls front lift in
    */
   public void frontLiftIn() {
     frontLiftL.set(DoubleSolenoid.Value.kReverse);
@@ -58,21 +63,24 @@ public class DrivetrainLift {
    * Pushes back lift out
    */
   public void backLiftOut() {
-    backLift.set(DoubleSolenoid.Value.kForward);
+    backLiftL.set(DoubleSolenoid.Value.kForward);
+    backLiftR.set(DoubleSolenoid.Value.kForward);
   }
 
   /**
-   * Pulles back lift in
+   * Pulls back lift in
    */
   public void backLiftIn() {
-    backLift.set(DoubleSolenoid.Value.kReverse);
+    backLiftL.set(DoubleSolenoid.Value.kReverse);
+    backLiftR.set(DoubleSolenoid.Value.kReverse);
   }
 
   /**
-   * Rotates the roller either forwards or backwards.
+   * Rotates the roller either forwards or backwards
+   *
    * @param speed the speed to rotate the roller motor (ranges from -1.0 to 1.0)
    */
-  public void roll(double speed) {
+  public void driveRoll(double speed) {
     rollerTalon.set(ControlMode.PercentOutput, speed);
   }
 }
