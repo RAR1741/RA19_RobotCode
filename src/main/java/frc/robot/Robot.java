d4ba7ddf75b4f0ec2f7a0d3bb8dce37324503cc1
Jordan Miller <jmiller@springbuk.com>
2019-02-11 18:56:08 -0500

    Updated from master

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -7,22 +7,37 @@
 
 package frc.robot;
 
-import frc.vision.*;
+import java.io.File;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.TimeZone;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import com.moandjiezana.toml.Toml;
 
 import org.opencv.core.Rect;
 import org.opencv.imgproc.Imgproc;
 
-import edu.wpi.first.cameraserver.CameraServer;
 import edu.wpi.cscore.UsbCamera;
+import edu.wpi.first.cameraserver.CameraServer;
+import edu.wpi.first.vision.VisionThread;
 import edu.wpi.first.wpilibj.AnalogInput;
 import edu.wpi.first.wpilibj.Compressor;
 import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.GenericHID.Hand;
+import edu.wpi.first.wpilibj.SPI.Port;
 import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.XboxController;
-import edu.wpi.first.vision.VisionThread;
-import java.util.logging.Logger;
+import frc.robot.logging.DataLogger;
+import frc.robot.Filesystem;
+import frc.robot.LoggableNavX;
+import frc.vision.MyVisionPipeline;
 
 /**
  * The VM is configured to automatically run this class, and to call the
@@ -32,200 +47,214 @@ import java.util.logging.Logger;
  * project.
  */
 public class Robot extends TimedRobot {
-    private static final Logger logger = Logger.getLogger(Robot.class.getName());
+  private static final Logger logger = Logger.getLogger(Robot.class.getName());
+  private Toml config;
 
-    private final int IMG_WIDTH = 640;
-    private final int IMG_HEIGHT = 480;
-    private UsbCamera camera;
-    private Compressor compressor;
-    private DoubleSolenoid ds1;
-    private DoubleSolenoid ds2;
-    private DoubleSolenoid ds3;
-    private DoubleSolenoid ds4;
-    private DoubleSolenoid ds5;
-    private DoubleSolenoid ds6;
-    private DoubleSolenoid ledLights;
-    private XboxController xbc;
-    private boolean xButtonState = false;
-    private boolean aButtonState = false;
-    private boolean bButtonState = false;
-    private boolean yButtonState = false;
-    private boolean bumperButtonState = false;
-    private boolean startButtonState = false;
-    private boolean backButtonState = false;
-    private DigitalInput leftLine;
-    private DigitalInput midLine;
-    private DigitalInput rightLine;
-    private PressureSensor pressureSensor;
+  private final int IMG_WIDTH = 640;
+  private final int IMG_HEIGHT = 480;
+  private UsbCamera camera;
+  private Compressor compressor;
+  private DigitalInput leftLine;
+  private DigitalInput midLine;
+  private DigitalInput rightLine;
+  private PressureSensor pressureSensor;
+  private XboxController xbc;
+  private Drivetrain drive;
+  private DataLogger dataLogger;
+  private LoggableNavX navX;
+  private UltrasonicSensor ultrasonicSensor;
+  private DoubleSolenoid ledLights;
+  private Timer timer;
 
-	private VisionThread visionThread;
-	private double centerX = 0.0;
+  private VisionThread visionThread;
+  private double centerX = 0.0;
 
-	private final Object imgLock = new Object();
+  private final Object imgLock = new Object();
 
-    /**
-     * This function is run when the robot is first started up and should be used
-     * for any initialization code.
-     */
-    @Override
-    public void robotInit() {
-        logger.info("Initializing robot...");
+  private void configureLogging() {
+    try {
+      Level logLevel = Level.parse(config.getString("log.level", "INFO"));
+      logger.setLevel(logLevel);
+    } catch (Exception ex) {
+      logger.severe(String.format("Couldn't set log level: %s", ex.getMessage()));
+    }
+  }
 
-        compressor = new Compressor();
-        compressor.start();
-        ds1 = new DoubleSolenoid(0, 0, 1);
-        ds2 = new DoubleSolenoid(0, 2, 3);
-        ds3 = new DoubleSolenoid(0, 4, 5);
-        ds4 = new DoubleSolenoid(0, 6, 7);
+  public void startDataLogging(String mode)
+  {
+    String dir = Filesystem.localPath("logs");
+    new File(dir).mkdirs();
+    TimeZone tz = TimeZone.getTimeZone("EST");
+    DateFormat df = new SimpleDateFormat("yyyy-MM-dd_HH-mm-ss");
+    df.setTimeZone(tz);
+    dataLogger.open(dir + "/log-" + df.format(new Date()) + "_" + mode + ".csv");
+    setupDataLogging();
+  }
 
-        ds5 = new DoubleSolenoid(1, 0, 1);
-        ds6 = new DoubleSolenoid(1, 2, 3);
+  private void log()
+  {
+    dataLogger.log("timer", timer.get());
+    dataLogger.log("leftLine", leftLine.get());
+    dataLogger.log("centerLine", midLine.get());
+    dataLogger.log("rightLine", rightLine.get());
+    dataLogger.logAll();
+    dataLogger.writeLine();
+  }
 
-        ledLights = new DoubleSolenoid(1, 4, 5);
-        ledLights.set(DoubleSolenoid.Value.kForward);
+  private void setupDataLogging()
+  {
+    dataLogger.addAttribute("timer");
+    dataLogger.addAttribute("leftLine");
+    dataLogger.addAttribute("centerLine");
+    dataLogger.addAttribute("rightLine");
+    dataLogger.addLoggable(drive);
+    dataLogger.addLoggable(navX);
+    dataLogger.setupLoggables();
+    dataLogger.writeAttributes();
+  }
 
+  /**
+   * This function is run when the robot is first started up and should be used
+   * for any initialization code.
+   */
+  @Override
+  public void robotInit() {
+    logger.info("Initializing robot...");
 
-        xbc = new XboxController(0);
+    logger.info("Starting timer...");
+    timer = new Timer();
+    timer.start();
+    logger.info("Timer started");
 
-        leftLine   = new DigitalInput(1);
-        midLine = new DigitalInput(2);
-        rightLine  = new DigitalInput(3);
-
-        camera = CameraServer.getInstance().startAutomaticCapture();
-        camera.setResolution(IMG_WIDTH, IMG_HEIGHT);
-
-        pressureSensor = new PressureSensor(new AnalogInput(0));
-
-        visionThread = new VisionThread(camera, new MyVisionPipeline(), pipeline -> {
-            if (!pipeline.filterContoursOutput().isEmpty()) {
-                Rect r = Imgproc.boundingRect(pipeline.filterContoursOutput().get(0));
-                synchronized (imgLock) {
-                    centerX = r.x + (r.width / 2);
-                    System.out.println("Camera: " + centerX);
-                }
-            }
-        });
-        visionThread.start();
-        logger.info("Robot initialized.");
+    try {
+      String pathToTomlFile = Filesystem.localDeployPath("robot.toml");
+      logger.info(String.format("Loading log file from \"%s\"", pathToTomlFile));
+      config = new Toml().read(new File(pathToTomlFile));
+    } catch (Exception ex) {
+      logger.severe(String.format("Couldn't load from file (falling back to empty): %s", ex.getMessage()));
+      config = new Toml();
     }
 
-    /**
-     * This function is called every robot packet, no matter the mode. Use this for
-     * items like diagnostics that you want ran during disabled, autonomous,
-     * teleoperated and test.
-     *
-     * <p>
-     * This runs after the mode specific periodic functions, but before LiveWindow
-     * and SmartDashboard integrated updating.
-     */
-    @Override
-    public void robotPeriodic() {
-        // This shouldn't be used yet.
-    }
+    logger.info("Starting drivetrain...");
+    drive = new Drivetrain(4,5,6,7);
+    logger.info("Drivetrain started");
 
-    /**
-     * This autonomous (along with the chooser code above) shows how to select
-     * between different autonomous modes using the dashboard. The sendable chooser
-     * code works with the Java SmartDashboard. If you prefer the LabVIEW Dashboard,
-     * remove all of the chooser code and uncomment the getString line to get the
-     * auto name from the text box below the Gyro
-     *
-     * <p>
-     * You can add additional auto modes by adding additional comparisons to the
-     * switch structure below with additional strings. If using the SendableChooser
-     * make sure to add them to the chooser code above as well.
-     */
-    @Override
-    public void autonomousInit() {
-        logger.info("Entering autonomous mode.");
+    configureLogging();
 
+    compressor = new Compressor(2);
+    compressor.start();
 
-    }
+    pressureSensor = new PressureSensor(new AnalogInput(0));
+    ultrasonicSensor = new UltrasonicSensor(new AnalogInput(1));
+    navX = new LoggableNavX(Port.kMXP);
 
-    /**
-     * This function is called periodically during autonomous.
-     */
-    @Override
-    public void autonomousPeriodic() {
-        // Run autonomous code (state machines, etc.)
-    }
+    xbc = new XboxController(0);
 
-    /**
-     * This function is called periodically during operator control.
-     */
-    @Override
-    public void teleopPeriodic() {
-        // Run teleop code (interpreting input, etc.)
-        System.out.println(String.format("Pressure: %2.2f", pressureSensor.getPressure()));
-        System.out.printf("Left: %b\n Middle: %b\n Right: %b\n",
-                          leftLine.get(), midLine.get(), rightLine.get());
-        if (xbc.getXButtonPressed()) {
-            xButtonState = !xButtonState;
-            if (xButtonState) {
-                ds1.set(DoubleSolenoid.Value.kForward);
-            } else {
-                ds1.set(DoubleSolenoid.Value.kReverse);
-            }
-        } else if (xbc.getAButtonPressed()) {
-            aButtonState = !aButtonState;
-            if (aButtonState) {
-                ds2.set(DoubleSolenoid.Value.kForward);
-            } else {
-                ds2.set(DoubleSolenoid.Value.kReverse);
-            }
-        } else if (xbc.getBButtonPressed()) {
-            bButtonState = !bButtonState;
-            if (bButtonState) {
-                ds3.set(DoubleSolenoid.Value.kForward);
-            } else {
-                ds3.set(DoubleSolenoid.Value.kReverse);
-            }
-        } else if (xbc.getYButtonPressed()) {
-            yButtonState = !yButtonState;
-            if (yButtonState) {
-                ds4.set(DoubleSolenoid.Value.kForward);
-            } else {
-                ds4.set(DoubleSolenoid.Value.kReverse);
-            }
-        } else if (xbc.getStartButtonPressed()) {
-            startButtonState = !startButtonState;
-            if (startButtonState) {
-                ds5.set(DoubleSolenoid.Value.kForward);
-            } else {
-                ds5.set(DoubleSolenoid.Value.kReverse);
-            }
-        } else if (xbc.getBackButtonPressed()) {
-            backButtonState = !backButtonState;
-            if (backButtonState) {
-                ds6.set(DoubleSolenoid.Value.kForward);
-            } else {
-                ds6.set(DoubleSolenoid.Value.kReverse);
-            }
-        } else if (xbc.getBumperPressed(Hand.kRight)) {
-            bumperButtonState = !bumperButtonState;
-            if (bumperButtonState) {
-                ds1.set(DoubleSolenoid.Value.kForward);
-                ds2.set(DoubleSolenoid.Value.kForward);
-                ds3.set(DoubleSolenoid.Value.kForward);
-                ds4.set(DoubleSolenoid.Value.kForward);
-                ds5.set(DoubleSolenoid.Value.kForward);
-                ds6.set(DoubleSolenoid.Value.kForward);
-            } else {
-                ds1.set(DoubleSolenoid.Value.kReverse);
-                ds2.set(DoubleSolenoid.Value.kReverse);
-                ds3.set(DoubleSolenoid.Value.kReverse);
-                ds4.set(DoubleSolenoid.Value.kReverse);
-                ds5.set(DoubleSolenoid.Value.kReverse);
-                ds6.set(DoubleSolenoid.Value.kReverse);
-            }
+    leftLine = new DigitalInput(1);
+    midLine = new DigitalInput(2);
+    rightLine = new DigitalInput(3);
+
+    // If we're not in the matrix...
+    if (!RuntimeDetector.isSimulation()) {
+      ledLights = new DoubleSolenoid(1, 4, 5);
+      ledLights.set(DoubleSolenoid.Value.kForward);
+
+      camera = CameraServer.getInstance().startAutomaticCapture();
+      camera.setResolution(IMG_WIDTH, IMG_HEIGHT);
+
+      visionThread = new VisionThread(camera, new MyVisionPipeline(), pipeline -> {
+        if (!pipeline.filterContoursOutput().isEmpty()) {
+          Rect r = Imgproc.boundingRect(pipeline.filterContoursOutput().get(0));
+          synchronized (imgLock) {
+            centerX = r.x + (r.width / 2);
+            System.out.println("Camera: " + centerX);
+          }
         }
+      });
+      visionThread.start();
     }
 
-    /**
-     * This function is called periodically during test mode.
-     */
-    @Override
-    public void testPeriodic() {
-        // This isn't typically used in our programs.
-    }
+    dataLogger = new DataLogger();
+    String pathToLogFile = Filesystem.localPath("logs", "log.csv");
+    dataLogger.open(pathToLogFile);
+    setupDataLogging();
+
+    logger.info("Robot initialized.");
+  }
+
+  /**
+   * This function is called every robot packet, no matter the mode. Use this for
+   * items like diagnostics that you want ran during disabled, autonomous,
+   * teleoperated and test.
+   *
+   * <p>
+   * This runs after the mode specific periodic functions, but before LiveWindow
+   * and SmartDashboard integrated updating.
+   */
+  @Override
+  public void robotPeriodic() {
+    // This shouldn't be used yet.
+  }
+
+  /**
+   * This autonomous (along with the chooser code above) shows how to select
+   * between different autonomous modes using the dashboard. The sendable chooser
+   * code works with the Java SmartDashboard. If you prefer the LabVIEW Dashboard,
+   * remove all of the chooser code and uncomment the getString line to get the
+   * auto name from the text box below the Gyro
+   *
+   * <p>
+   * You can add additional auto modes by adding additional comparisons to the
+   * switch structure below with additional strings. If using the SendableChooser
+   * make sure to add them to the chooser code above as well.
+   */
+  @Override
+  public void autonomousInit() {
+    logger.info("Entering autonomous mode.");
+    startDataLogging("auto");
+  }
+
+  /**
+   * This function is called periodically during autonomous.
+   */
+  @Override
+  public void autonomousPeriodic() {
+    // Run autonomous code (state machines, etc.)
+    log();
+  }
+
+  @Override
+  public void teleopInit() {
+    startDataLogging("teleop");
+  }
+
+  /**
+   * This function is called periodically during operator control.
+   */
+  @Override
+  public void teleopPeriodic() {
+    // Run teleop code (interpreting input, etc.)
+    // System.out.println(String.format("Pressure: %2.2f", pressureSensor.getPressure()));
+    drive.arcadeDrive(xbc.getX(GenericHID.Hand.kLeft),
+                      xbc.getY(GenericHID.Hand.kLeft));
+    log();
+  }
+
+  @Override
+  public void testInit() {
+    startDataLogging("test");
+  }
+
+  /**
+   * This function is called periodically during test mode.
+   */
+  @Override
+  public void testPeriodic() {
+    log();
+  }
+
+  @Override
+  public void disabledInit() {
+    dataLogger.close();
+  }
 }
