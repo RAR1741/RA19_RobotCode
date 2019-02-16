29344ebb3a94abc3aac1349c414a07a0de6fbcd4
Patrick Frampton <pframpton@salesforce.com>
2019-02-16 12:48:05 -0500

    Check status of limit switches

--- src/main/java/frc/robot/Manipulation.java
+++ src/main/java/frc/robot/Manipulation.java
@@ -6,6 +6,7 @@ import frc.robot.logging.DataLogger;
 import frc.robot.logging.Loggable;
 
 import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.Faults;
 
 public class Manipulation implements Loggable {
   private WPI_TalonSRX liftTalon;
@@ -33,11 +34,17 @@ public class Manipulation implements Loggable {
     dl.addAttribute("liftInput");
     dl.addAttribute("liftCurrent");
     dl.addAttribute("liftPosition");
+    dl.addAttribute("liftFwdLimit");
+    dl.addAttribute("liftRevLimit");
   }
 
   public void log(DataLogger dl) {
+    Faults faults = new Faults();
+    liftTalon.getFaults(faults);
     dl.log("liftInput", liftTalon.get());
     dl.log("liftCurrent", liftTalon.getOutputCurrent());
     dl.log("liftPosition", liftTalon.getSelectedSensorPosition());
+    dl.log("liftFwdLimit", faults.ForwardLimitSwitch);
+    dl.log("liftRevLimit", faults.ReverseLimitSwitch);
   }
 }
