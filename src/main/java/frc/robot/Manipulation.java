8652a4e08e0c3388a6c186bc72d28a7a3e841695
Patrick Frampton <pframpton@salesforce.com>
2019-02-16 12:21:34 -0500

    Wire up manipulation

--- src/main/java/frc/robot/Manipulation.java
+++ src/main/java/frc/robot/Manipulation.java
@@ -4,22 +4,22 @@ import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 import com.ctre.phoenix.motorcontrol.ControlMode;
 
 public class Manipulation {
-    private WPI_TalonSRX liftTalon 
-    
-    /**
-     * Constructor
-     *
-     * @param liftCanId The CAN id of the talon for the lift motor
-     */
-    Manipulation (int liftCanId) {
-        liftTalon = new WPI_TalonSRX(liftCanId);
-    }
+  private WPI_TalonSRX liftTalon;
 
-    /**
-     * Lifts the lift either up or down.
-     * @param speed the speed to rotate the motor (ranges from -1.0 to 1.0)
-     */
-    public void lift(double speed) {
-        liftTalon.set(ControlMode.PercentOutput, speed);
-    }
+  /**
+   * Constructor
+   *
+   * @param liftCanId The CAN id of the talon for the lift motor
+   */
+  Manipulation (int liftCanId) {
+    liftTalon = new WPI_TalonSRX(liftCanId);
+  }
+
+  /**
+   * Lifts the lift either up or down.
+   * @param speed the speed to rotate the motor (ranges from -1.0 to 1.0)
+   */
+  public void lift(double speed) {
+    liftTalon.set(ControlMode.PercentOutput, speed);
+  }
 }
