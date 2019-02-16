2bd4a3cd0ac0e6b86a4173113a09d8eb61f02fb1
Patrick Frampton <pframpton@salesforce.com>
2019-02-16 15:01:48 -0500

    Clean up scoring with new design

--- src/main/java/frc/robot/Scoring.java
+++ src/main/java/frc/robot/Scoring.java
@@ -1,60 +1,75 @@
 package frc.robot;
 
 import edu.wpi.first.wpilibj.DoubleSolenoid;
-import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+import frc.robot.loggable.LoggableTalonSRX;
+import frc.robot.logging.DataLogger;
+import frc.robot.logging.Loggable;
+
 import com.ctre.phoenix.motorcontrol.ControlMode;
 
 /**
  * Class for the scoring subsystem used by the 2019 robot
  */
-public class Scoring {
-    private WPI_TalonSRX rollerTalon;
-    private WPI_TalonSRX tiltTalon;
-    private DoubleSolenoid pushers;
+public class Scoring implements Loggable {
+  private LoggableTalonSRX rollerTalon;
+  private LoggableTalonSRX wristTalon;
+  private DoubleSolenoid pushers;
 
-    /**
-     * Constructor
-     *
-     * @param rollerCanId     The CAN id of the talon for the roller motor
-     * @param tiltCanId     The CAN id of the talon for the tilt motor
-     *     *
-     * @param pushersCanId    The CAN id of the pcm for the pushers
-     * @param pushersChannel1 The channel number of the forward channel for the pushers
-     * @param pushersChannel2 The channel number of the reverse channel for the pushers
-     */
-    Scoring(int rollerCanId, int tiltCanId, int pushersCanId, int pushersChannel1, int pushersChannel2) {
-        rollerTalon    = new WPI_TalonSRX(rollerCanId);
-        tiltTalon   = new WPI_TalonSRX(tiltCanId);
-        pushers        = new DoubleSolenoid(pushersCanId, pushersChannel1, pushersChannel2);
-    }
+  /**
+   * Constructor
+   *
+   * @param roller  The talon for the roller
+   * @param wrist   The talon for the wrist motor
+   * @param pushers The double solenoid for the pushers
+   */
+  Scoring(LoggableTalonSRX roller, LoggableTalonSRX wrist, DoubleSolenoid pushers) {
+    rollerTalon = roller;
+    wristTalon = wrist;
+    this.pushers = pushers;
 
-    /**
-     * Rotates the roller either forwards or backwards.
-     * @param speed the speed to rotate the roller motor (ranges from -1.0 to 1.0)
-     */
-    public void roll(double speed) {
-        rollerTalon.set(ControlMode.PercentOutput, speed);
-    }
+    rollerTalon.setName("Roller");
+    wristTalon.setName("Wrist");
+    this.pushers.setName("Pushers");
+  }
 
-    /**
-     * Pushes the pneumatic actuators forwards.
-     */
-    public void intakeDown() {
-        pushers.set(DoubleSolenoid.Value.kForward);
-    }
+  /**
+   * Rotates the roller either forwards or backwards.
+   *
+   * @param speed the speed to rotate the roller motor (ranges from -1.0 to 1.0)
+   */
+  public void roll(double speed) {
+    rollerTalon.set(ControlMode.PercentOutput, speed);
+  }
 
-    /**
-     * Pushes the pneumatic actuators back in
-     */
-    public void intakeUp() {
-        pushers.set(DoubleSolenoid.Value.kReverse);
-    }
+  /**
+   * Pushes the pneumatic actuators forwards.
+   */
+  public void intakeDown() {
+    pushers.set(DoubleSolenoid.Value.kForward);
+  }
 
-    /**
-     * Rotates the tilt motor
-     * @param speed the speed to rotate the tilter (ranges from -1.0 to 1.0)
-     */
-    public void tilt(double speed) {
-        tiltTalon.set(ControlMode.PercentOutput, speed);
-    }
+  /**
+   * Pushes the pneumatic actuators back in
+   */
+  public void intakeUp() {
+    pushers.set(DoubleSolenoid.Value.kReverse);
+  }
+
+  /**
+   * Rotates the wrist motor
+   *
+   * @param speed the speed to rotate the wrist (ranges from -1.0 to 1.0)
+   */
+  public void tilt(double speed) {
+    wristTalon.set(ControlMode.PercentOutput, speed);
+  }
+
+  public void setupLogging(DataLogger dl) {
+    dl.addLoggable(wristTalon);
+    dl.addLoggable(rollerTalon);
+  }
+
+  public void log(DataLogger dl) {
+    // No-op, logAll should handle loggables
+  }
 }
