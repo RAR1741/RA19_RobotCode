9c0c3b0e9f6f7af9fa89772659b81a9bed4abdda
ultragamer1010 <31806444+ultragamer1010@users.noreply.github.com>
2019-02-21 20:13:57 -0500

    Add methods for lifting to different scoring levels

--- src/main/java/frc/robot/Manipulation.java
+++ src/main/java/frc/robot/Manipulation.java
@@ -9,6 +9,14 @@ import com.ctre.phoenix.motorcontrol.ControlMode;
 public class Manipulation implements Loggable {
   private LoggableTalonSRX liftTalon;
 
+  //Lift positions need to be determined
+  private final float LIFTLEVEL1 = 0;
+  private final float LIFTLEVEL2 = 0;
+  private final float LIFTLEVEL3 = 0;
+
+  private double P;
+  private double error;
+
   /**
    * Constructor
    *
@@ -26,6 +34,41 @@ public class Manipulation implements Loggable {
    */
   public void lift(double speed) {
     liftTalon.set(ControlMode.PercentOutput, -speed);
+  }
+
+  /**
+   * Uses P control to move the lift.
+   *
+   * @param error error from the target
+   * @param maxMotorPercent maximum motor percent
+   */
+  public void liftPControl(double error, double maxMotorPercent) {
+    double motorPower = maxMotorPercent * error;
+    liftTalon.set(motorPower);
+  }
+
+  /**
+   * Lifts lift to level 1.
+   */
+  public void level1() {
+    error = (LIFTLEVEL1 - liftTalon.getSelectedSensorPosition()) * P;
+    liftPControl(error, 0.8);
+  }
+
+  /**
+   * Lifts lift to level 2.
+   */
+  public void level2() {
+    error = (LIFTLEVEL2 - liftTalon.getSelectedSensorPosition()) * P;
+    liftPControl(error, 0.8);
+  }
+
+  /**
+   * Lifts lift to level 3.
+   */
+  public void level3() {
+    error = (LIFTLEVEL3 - liftTalon.getSelectedSensorPosition()) * P;
+    liftPControl(error, 0.8);
   }
 
   public void setupLogging(DataLogger dl) {
