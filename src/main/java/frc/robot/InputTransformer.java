c303353779810485c466308a92e7eb8a0b1b48a9
Programmer <programmer@redalert1741.org>
2019-03-06 19:34:17 -0500

    Default to 0.6 for scale

--- src/main/java/frc/robot/InputTransformer.java
+++ src/main/java/frc/robot/InputTransformer.java
@@ -6,11 +6,11 @@ public class InputTransformer implements Configurable {
   private double maxTurn;
 
   public InputTransformer() {
-    maxTurn = 1.0;
+    maxTurn = 0.6;
   }
 
   public void configure(Toml config) {
-    maxTurn = config.getDouble("input.maxTurn", 1.0);
+    maxTurn = config.getDouble("input.maxTurn", 0.6);
   }
 
   double transformDrive(double input) {
