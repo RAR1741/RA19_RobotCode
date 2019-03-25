f8e8647b8b7464b945123afb2a3332035d89749e
Programmer <programmer@redalert1741.org>
2019-03-25 19:33:09 -0400

    Fix comp bot

--- src/main/java/frc/robot/Manipulation.java
+++ src/main/java/frc/robot/Manipulation.java
@@ -31,7 +31,7 @@ public class Manipulation implements Loggable, Configurable {
    * @param speed the speed to rotate the motor (ranges from -1.0 to 1.0)
    */
   public void lift(double speed) {
-    liftTalon.set(ControlMode.PercentOutput, -speed);
+    liftTalon.set(ControlMode.PercentOutput, speed);
   }
 
   /**
